fb4d5aabb8adb31a3f5feda6adf56fb2dbf4b31d
jslben <jleeae@connect.ust.hk>
2019-03-26 01:02:59 +0800

    modified

--- src/js/include_sections.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $.get('html/about.html',function(data){ $('#p_about').html(data);});
-  $.get('html/education.html',function(data){ $('#s_education').html(data);});
-  $.get('html/research.html',function(data){ $('#s_research').html(data);});
-  $.get('html/work_experience.html',function(data){ $('#s_work').html(data);});
-  $.get('html/teaching.html',function(data){ $('#s_teaching').html(data);});
-  $.get('html/grants_awards_skills.html',function(data){ $('#s_grants_awards_skills').html(data);});
-});
